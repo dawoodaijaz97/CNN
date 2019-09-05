8a39d3d18e3f026772df71a83f471d77b1fab439
Dawood <dawood.aijaz97@gmail.com>
2019-09-05 18:04:30 +0500

    #

--- index.py
+++ index.py
@@ -7,8 +7,7 @@ from keras.preprocessing.image import ImageDataGenerator
 
 classifier = Sequential()
 
-classifier.add(Convolution2D(filters=32, kernel_size=(3, 3), input_shape=(64, 64, 3), activation="relu"))
-
+classifier.add(Convolution2D(filters=32, kernel_size=(3, 3), input_shape=(32, 32, 3), activation="relu"))
 classifier.add(MaxPool2D(2, 2))
 
 classifier.add(Convolution2D(filters=32, kernel_size=(3, 3), activation="relu"))
@@ -21,25 +20,25 @@ classifier.add(Dense(units=1, activation="sigmoid"))
 
 classifier.compile(optimizer="adam",loss="binary_crossentropy",metrics=["accuracy"])
 
-train_datagen = ImageDataGenerator(rescale=1. / 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+train_datagen = ImageDataGenerator(rescale=1./ 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
 
-test_datagen = ImageDataGenerator(rescale=1. / 255)
+test_datagen = ImageDataGenerator(rescale=1./ 255)
 
 training_set = train_datagen.flow_from_directory(
     'dataset/training_set',
-    target_size=(64, 64),
+    target_size=(32, 32),
     batch_size=32,
     class_mode='binary')
 
 test_set = test_datagen.flow_from_directory(
     'dataset/test_set',
-    target_size=(64, 64),
+    target_size=(32, 32),
     batch_size=32,
     class_mode='binary')
 
 classifier.fit_generator(
     training_set,
     steps_per_epoch=8000,
-    epochs=25,
+    epochs=15,
     validation_data=test_set,
     validation_steps=2000)
